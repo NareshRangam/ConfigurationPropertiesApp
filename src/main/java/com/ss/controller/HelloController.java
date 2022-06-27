4393c606e0e320db9deded3101c1603ad2ae1724
NareshRangam <nareshrangam99@gmail.com>
2022-06-27 16:34:33 +0530

    change

---
+++ src/main/java/com/ss/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.ss.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/example/string")
+	public ResponseEntity<String> exampleString() {
+	  return ResponseEntity.ok("String example here");
+	}
+
+}
